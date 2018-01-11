158ff009990c1e23f64dc1fee6762ef29666d363
John Griffith <john.griffith@live.co.uk>
2018-01-11 00:40:13 +0000

    Updated epoch format

--- main.py
+++ main.py
@@ -271,7 +271,7 @@ def main(_):
                 total_validation_accuracy += validation_accuracy
                 validation_batches += 1
             validation_accuracy = total_validation_accuracy / validation_batches
-            print('epoch {}, accuracy on validation set : {}'.format(step + 1, validation_accuracy))
+            print('epoch %02d, accuracy on validation set : %.3f' % (step + 1, validation_accuracy))
             if validation_accuracy >= best_accuracy:
                 best_saver.save(sess, best_model_path)
                 best_accuracy = validation_accuracy
@@ -308,7 +308,7 @@ def main(_):
             evaluated_images += len(testLabels)
 
         test_accuracy = test_accuracy / batch_count
-        print('test set: accuracy on test set: %0.3f' % test_accuracy)
+        print('test set: accuracy on test set: %.3f' % test_accuracy)
         print('model saved to ' + checkpoint_path)
 
         train_writer.close()
